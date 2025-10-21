1a14cf2f7d177539dcadd57ba33bd65e59690865
yadirahmat_ <yaddrahmat@gmail.com>
2025-10-21 15:51:19 +0700

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist', // direktori output build
+    assetsDir: 'assets', // folder aset statis
+    rollupOptions: {
+      input: 'index.html', // pastikan file utama benar
+    },
+  },
+  base: './', // penting agar path relatif di Vercel
+  server: {
+    host: true,
+    port: 5173,
+  },
 })
