e33f76084b7421306a328f3e3b8c057fe4823952
yadirahmat_ <yaddrahmat@gmail.com>
2025-11-05 21:22:34 +0700

    fix vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,28 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   build: {
+//     outDir: 'dist', // direktori output build
+//     assetsDir: 'assets', // folder aset statis
+//     rollupOptions: {
+//       input: 'index.html', // pastikan file utama benar
+//     },
+//   },
+//   base: './', // penting agar path relatif di Vercel
+//   server: {
+//     host: true,
+//     port: 5173,
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
@@ -16,7 +41,6 @@ export default defineConfig({
       input: 'index.html', // pastikan file utama benar
     },
   },
-  base: './', // penting agar path relatif di Vercel
   server: {
     host: true,
     port: 5173,
